c4503c960b96df569b4f8d2bdbc237037c312bd7
Amon Peide Ng <peide.amon.ng@gmail.com>
2018-07-12 19:49:45 +1200

    removed all

--- common.js
+++
@@ -1,9 +0,0 @@
-var env = require('./env').env;
-
-
-console.log(env["development"]);
-
-exports.config = function() {
-  var node_env = process.env.NODE_ENV || 'development';
-  return env[node_env];
-};
--- env.js
+++
@@ -1,8 +0,0 @@
-exports.env = {
-  "development": {
-      "db": "mongodb://localhost:27017/"
-  }, 
-  "production": {
-      "db": "mongodb://admin:080Bumithai793@ds233581.mlab.com:33581/heroku_88vd0qwv",
-  }
-};

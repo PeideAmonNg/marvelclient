108bd61fed4254f39e701ca40f72712255125682
Amon Peide Ng <peide.amon.ng@gmail.com>
2018-07-12 20:21:35 +1200

    add logging to debug mongo connection path

--- common.js
+++ common.js
@@ -2,6 +2,8 @@ var env = require('./env').env;
 
 
 console.log(env["development"]);
+console.log(process.env.NODE_ENV);
+console.log(env[process.env.NODE_ENV]);
 
 exports.config = function() {
   var node_env = process.env.NODE_ENV || 'development';

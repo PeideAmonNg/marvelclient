4c52272c733e0a06d46c7f669d4e9ccf42989fb9
Amon Peide Ng <peide.amon.ng@gmail.com>
2018-07-12 20:32:06 +1200

    remove slash in db path

--- env.js
+++ env.js
@@ -3,6 +3,6 @@ exports.env = {
 		db: "mongodb://localhost:27017/"
 	},
 	production: {
-		db: "mongodb://admin1:admin1spassword@ds133601.mlab.com:33601/heroku_cf7f9xz3/"
+		db: "mongodb://admin1:admin1spassword@ds133601.mlab.com:33601/heroku_cf7f9xz3"
 	}
 }
